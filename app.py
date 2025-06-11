a3335cc6556f2d2ca545c899905363235d9610a7
equipocostos <equipocostosminedu@gmail.com>
2025-06-11 16:14:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 import joblib
 
 app = Flask(__name__)
@@ -14,3 +15,7 @@ def predict():
     texto = data.get('texto', '')
     pred = modelo.predict([texto])[0]
     return jsonify({'corregido': pred})
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Importante para Render
+    app.run(host='0.0.0.0', port=port)
