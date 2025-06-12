b72794d3b3f5d2ee15ea3baf342628419e8630ea
equipocostos <equipocostosminedu@gmail.com>
2025-06-12 11:58:02 -0500

    Modelo actualizado con UE

--- app.py
+++ app.py
@@ -2,20 +2,27 @@
 from flask import Flask, request, jsonify
 import os
 import joblib
+import pandas as pd
 
 app = Flask(__name__)
 modelo = joblib.load('modelo.pkl')
+mapeo = pd.read_csv('mapeo_centros.csv')
 
 @app.route('/')
 def home():
-    return 'Microservicio de corrección de oficinas'
+    return 'Microservicio IA Centros de Costos'
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
     texto = data.get('texto', '')
     pred = modelo.predict([texto])[0]
-    return jsonify({'corregido': pred})
+    resultado = mapeo[mapeo['centro_de_costo'] == pred].iloc[0].to_dict()
+    return jsonify({
+        'centro_de_costo': resultado['centro_de_costo'],
+        'codigo_centro_costo': resultado['codigo_centro_costo'],
+        'ue': resultado['ue']
+    })
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
